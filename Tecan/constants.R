1f7ca1525eace930c963a90c9956b6f9db0d3190
Ploulackk <ploulackk@test.com>
2017-08-23 11:19:07 -0400

    Upd

--- Tecan/constants.R
+++
@@ -1,4 +0,0 @@
-beers_constants <- as_tibble(list(Name = c("Molar Absorbance Co-efficient", "Path Length"),
-                                      Unit = c("ug/ml*cm", "cm"),
-                                      Value = c(.02, .19)))
-
