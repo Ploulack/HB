9cbd5ac37be15b78072eaf43b873ef9adf566226
Mathieu Castelli <mathieu.castelli@c4mprod.com>
2018-01-01 16:41:22 -0500

    minor

--- Tecan/ui.R
+++ Tecan/ui.R
@@ -10,7 +10,7 @@ source("hamilton/hamilton.R")
 display_tab <- function(panel_name, btn_label, module_ui_name) {
         btn_id <- paste0(module_ui_name, "_go")
         condition_string <- paste0("input.", btn_id)
-        module_function <- paste0(module_ui_name, "_ui") %>% rlang::as_function()
+        module_function <- rlang::as_function(paste0(module_ui_name, "_ui"))
         
         tabPanel(panel_name,
                  conditionalPanel(condition = "output.token_exists == true",
@@ -25,27 +25,24 @@ display_tab <- function(panel_name, btn_label, module_ui_name) {
                  )
         )
 }
-
-fluidPage(
-        tabsetPanel(id = "tab",
-                    conditionalPanel(
-                            condition = "output.token_exists == false",
-                            google_auth_button_ui("Auth")),
-                    
-                    display_tab(panel_name = "Tecan",
-                                btn_label = "Launch Tecan tool",
-                                module_ui_name = "tecan"),
-                    
-                    display_tab(panel_name = "Order DNA",
-                                btn_label = "Launch Order DNA tool",
-                                module_ui_name = "decision"),
-                    
-                    display_tab(panel_name = "Gel",
-                                btn_label = "Start Gel viewer",
-                                module_ui_name = "gel"),
-                    
-                    display_tab(panel_name = "Hamilton",
-                                btn_label = "Start Hamilton editor",
-                                module_ui_name = "hami")
-        )        
-)
+tabsetPanel(id = "tab",
+            conditionalPanel(
+                    condition = "output.token_exists == false",
+                    google_auth_button_ui("Auth")),
+            
+            display_tab(panel_name = "Tecan",
+                        btn_label = "Launch Tecan tool",
+                        module_ui_name = "tecan"),
+            
+            display_tab(panel_name = "Order DNA",
+                        btn_label = "Launch Order DNA tool",
+                        module_ui_name = "decision"),
+            
+            display_tab(panel_name = "Gel",
+                        btn_label = "Start Gel viewer",
+                        module_ui_name = "gel"),
+            
+            display_tab(panel_name = "Hamilton",
+                        btn_label = "Start Hamilton editor",
+                        module_ui_name = "hami")
+)        
